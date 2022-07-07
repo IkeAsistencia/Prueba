e524cf846e34c18f7f6b75347fda79d7ca6e0383
edgcuevas <91091527+edgcuevas@users.noreply.github.com>
2022-07-07 12:37:32 -0500

    Update HelloController.java

--- src/main/java/com/docker/DockerDemo/controller/HelloController.java
+++ src/main/java/com/docker/DockerDemo/controller/HelloController.java
@@ -1,14 +1,20 @@
-package com.javatutoriales.stringboot.tutorial1.controllers;
+package com.docker.DockerDemo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
-public class HelloWorldController {
- 
-    @GetMapping
-    public String saludos(@RequestParam(required = false, defaultValue = "") String nombre) {
-        return "".equals(nombre) ? "Hola Mundo!" : "Hola " + nombre;
-    }
+import com.docker.DockerDemo.entities.Greeting;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping(path = "/greeting", method = RequestMethod.GET)
+	public Greeting sayHello(@RequestParam String name) {
+		String message = "Welcome to Amazon Its time we change " + name;
+		Greeting dto = new Greeting();
+		dto.setMessage(message);
+		return dto;
+	}
 }
