69ff56afeb43d47b1ea5abf5a8a6a0461b2178f9
IkeAsistencia <89940239+IkeAsistencia@users.noreply.github.com>
2022-07-07 12:34:20 -0500

    Update HelloController.java

--- src/main/java/com/docker/DockerDemo/controller/HelloController.java
+++ src/main/java/com/docker/DockerDemo/controller/HelloController.java
@@ -1,13 +1,8 @@
-package com.docker.DockerDemo.controller;
-
+package com.javatutoriales.stringboot.tutorial1.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.docker.DockerDemo.entities.Greeting;
 
 @RestController 
 public class HelloWorldController {
